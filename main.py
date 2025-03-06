b4d1d2740a274dda70fda3bdd024c429805a6942
Jonathas <114630135+Jhonyc92@users.noreply.github.com>
2025-03-06 00:26:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import time
 
+from init.py import __init__()
+
+from alimentar.py import alimentar()
+
+from brincar.py import brincar()
+
+from atualizar_humor.py import atualizar_humor()
+
+from status.py import status()
+
+from menu.py import menu()
+
 meu_bichinho.fome += 5
 
 meu_bichinho.saude -= 5
