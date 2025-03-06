7f39c63b176919d8e77b296ce48ae2cc136f27d3
Jonathas <114630135+Jhonyc92@users.noreply.github.com>
2025-03-05 23:00:19 -0300

    Create init.py

---
+++ init.py
@@ -0,0 +1,11 @@
+class BichinhoVirtual:
+    
+    def __init__(self, nome, fome = 50, saude = 50, humor = 100):
+        
+        self.nome = nome
+        
+        self.fome = fome
+        
+        self.saude = saude
+        
+        self.humor = humor
